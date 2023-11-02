210ab865cccd5db8926421435c5ac679863cf8e7
HeyabD <124327412+HeyabD@users.noreply.github.com>
2023-11-01 22:09:43 -0400

    Update SpotOn.sql

--- sql/SpotOn.sql
+++ sql/SpotOn.sql
@@ -1,38 +1,63 @@
-DROP TABLE IF EXISTS LogIn;
-DROP TABLE IF EXISTS Playlist;
-DROP TABLE IF EXISTS Song;
 DROP TABLE IF EXISTS PlaylistSong;
 
-CREATE TABLE LogIn (
-	emailAddress varchar(50) PRIMARY KEY,
-	password varchar(30)
-	);
+DROP TABLE IF EXISTS Playlist;
 
-CREATE TABLE Playlist (
-playlistID integer PRIMARY KEY,
+DROP TABLE IF EXISTS Song;
+
+DROP TABLE IF EXISTS LogIn;
+
+CREATE TABLE
+    LogIn (
+        emailAddress varchar(50) PRIMARY KEY,
+        password varchar(30)
+    );
+
+CREATE TABLE
+    Playlist (
+        playlistID integer PRIMARY KEY,
         name varchar(50),
-	 
-	spotifyLink varchar(100) NOT NULL,
-	email varchar(50) REFERENCES LogIn(emailAddress)
-	);
+        spotifyLink varchar(100) NOT NULL,
+        email varchar(50) REFERENCES LogIn(emailAddress)
+    );
 
-CREATE TABLE Song(songID integer PRIMARY KEY,
-          title varchar(50), 
-           artist varchar(50),
-duration integer
-);
+CREATE TABLE
+    Song(
+        songID integer PRIMARY KEY,
+        title varchar(50),
+        artist varchar(50),
+        duration integer
+    );
 
-CREATE TABLE PlaylistSong (
-	playlist_ID integer REFERENCES Playlist(playlistID), 
-	song_ID integer REFERENCES Song(songID)
-	);
+CREATE TABLE
+    PlaylistSong(
+        playlist_ID integer REFERENCES Playlist(playlistID),
+        song_ID integer REFERENCES Song(songID),
+        PRIMARY KEY (playlist_ID, song_ID)
+    );
 
 INSERT INTO LogIn VALUES ('heyab.robel@gmail.com', 'password123');
 
+INSERT INTO
+    Playlist(
+        playlistID,
+        name,
+        email,
+        spotifylink
+    )
+VALUES (
+        1,
+        'First',
+        'heyab.robel@gmail.com',
+        'https://spotify.com'
+    );
 
-INSERT INTO Playlist(playlistID, name, email) VALUES (1, 'First', 'heyab.robel@gmail.com');
+INSERT INTO
+    Song(songID, title, artist, duration)
+VALUES (
+        1,
+        'Song Title',
+        'Artist Name',
+        240
+    );
 
-INSERT INTO Song VALUES (1, 'me@calvin.edu');
-
-
-INSERT INTO PlayerGame VALUES (1, 1, 0.00);
+INSERT INTO PlaylistSong(playlist_ID, song_ID) VALUES (1, 1);
